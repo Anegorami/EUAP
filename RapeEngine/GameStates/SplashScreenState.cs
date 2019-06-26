31ac4563d5c84e8080363bd9a95efd372c36ce4a
Drue Satterfield <druesatterfield@gmail.com>
2019-06-25 22:14:46 -0700

    added basic class for splash screen

---
+++ RapeEngine/GameStates/SplashScreenState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapeEngine.GameStates
+{
+    public class SplashScreenState: IGameObject
+    {
+        StateSystemManager stateManager;
+
+        public SplashScreenState(StateSystemManager stateSystemManager)
+        {
+            stateManager = stateSystemManager;
+        }
+        public void Update(double elapsedTimeMs)
+        {
+
+        }
+
+        public void Render()
+        {
+
+        }
+    }
+}
