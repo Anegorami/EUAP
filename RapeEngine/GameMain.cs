6f855885b4929d18d1ba4224eed3b7b2ca32d73a
Drue Satterfield <druesatterfield@gmail.com>
2019-06-25 22:15:20 -0700

    updated gameMain with state system manager content

--- RapeEngine/GameMain.cs
+++ RapeEngine/GameMain.cs
@@ -1,4 +1,5 @@
-﻿using SharpGL;
+﻿using RapeEngine.GameStates;
+using SharpGL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,18 +14,22 @@ namespace RapeEngine
         static private GameLoopExecutor gameLoopExecutor;
         static private OpenGL gl;
         static private OpenGLControl glGUI_Element;
+        static private StateSystemManager stateManager;
 
         static public void GameMainBegin(OpenGLControl glControl, StateSystemManager stateSystemManager)
         {
             gl = glControl.OpenGL;
             glGUI_Element = glControl;
-        }
+            stateManager = stateSystemManager;
+
+            gl.ClearColor(0, 0, 0, 1);
 
             gameLoopExecutor = new GameLoopExecutor(GameLoop);
         }
 
         static void GameLoop(double elapsedTimeMs)
         {
+            stateManager.Update(elapsedTimeMs);
 
             if (glGUI_Element.RenderTrigger == RenderTrigger.Manual)
             {
